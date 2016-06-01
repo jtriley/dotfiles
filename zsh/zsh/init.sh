171e769b158f8dee259eb5c258433dfeb6aecd88
Justin Riley <justin.t.riley@gmail.com>
2016-06-01 10:50:45 -0400

    add support for loading configs before omz init

--- zsh/zsh/init.sh
+++ zsh/zsh/init.sh
@@ -3,6 +3,9 @@
 # add a function path
 fpath=($ZSH/functions $fpath)
 
+# Load all of the $PATH files
+for preload_file ($ZSH/preloads/*.zsh) source $preload_file
+
 # Load all of the config files in ~/oh-my-zsh that end in .zsh
 # TIP: Add files you don't want in git to .gitignore
 for config_file ($ZSH/lib/*.zsh) source $config_file
